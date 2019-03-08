d308d145bde45a9a90bef1b0f514707a67419e4c
Luisa158 <luisa.monsalve.2019@upb.edu.co>
2019-03-08 10:33:19 -0500

    Mi primer commit

---
+++ primos.py
@@ -0,0 +1,9 @@
+
+x=int(input("Por favor, ingrese un número: "))
+y=int(input("Por favor, ingrese otro número: "))
+
+if x%y==0:
+    print("División exacta")
+
+else:
+    print("No es división exacta")
